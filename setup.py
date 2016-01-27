36115cee70408e27c5c2db10d2bd997c08c8e94a
denysboiko <denys.bojko@gmail.com>
2016-01-28 00:07:21 +0200

    Initial Testing

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+setup(name='Dceta',
+      version='2.0',
+      description='Dceta Studio Website',
+      author='Dceta Studio',
+      author_email='contact@dceta.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Django>=1.3'],
      )
